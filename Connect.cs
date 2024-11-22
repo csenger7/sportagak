3adf1b757d001da421d671e3721d2c83e32ecda8
csenger7 <lengyelcs@kkszki.hu>
2024-11-22 12:50:50 +0100

    Add project files.

---
+++ Connect.cs
@@ -0,0 +1,28 @@
+﻿using MySql.Data.MySqlClient;
+using System.Data.SqlClient;
+
+namespace sportesemény
+{
+    public class Connect
+    {
+        public MySqlConnection Connection;
+        public string Host;
+        public string Database;
+        public string User;
+        public string Password;
+        public string ConnectionString;
+
+        public Connect()
+        {
+            Host = "localhost";
+            Database = "Team";
+            User = "root";
+            Password = "";
+
+            ConnectionString = "SERVER=" + Host + ";DATABASE=" + Database + ";UID=" + User + ";PASSWORD=" + Password + ";SslMode=None";
+
+            Connection = new MySqlConnection(ConnectionString);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using sportesemény;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sportesemény
+{
+    internal class Program
+    {
+        static Connect conn = new Connect();
+
+
+        public static void GetAllData()
+        {
+            conn.Connection.Open();
+            string sql = "SELECT * FROM Users";
+            MySqlCommand cmd = new MySqlCommand(sql, conn.Connection);
+            MySqlDataReader dr = cmd.ExecuteReader();
+
+            Console.WriteLine("Felhasználók listája:");
+
+            while (dr.Read())
+            {
+                var user = new
+                {
+                    Id = dr.GetInt32(0),
+                    UserName = dr.GetString(1),
+                    Email = dr.GetString(2),
+                    RegistrationTime = dr.GetDateTime(4),
+                    UpdatedTime = dr.GetDateTime(5)
+                };
+
+                Console.WriteLine($"ID: {user.Id}, Név: {user.UserName}, Sportág: {user.}, Regisztrált: {user.RegistrationTime}, Módosítva: {user.UpdatedTime}");
+            }
+
+            dr.Close();
+            conn.Connection.Close();
+        }
+
+        static void Main(string[] args)
+        {
+            GetAllData();
+        }
+    }
+}
